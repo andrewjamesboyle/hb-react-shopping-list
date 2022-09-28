c987486ecdc3d1a80807b4fe3e4eb5900b868243
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-28 13:12:16 -0700

    items page renders conditionally

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import Header from './components/Header';
 import Items from './components/Items/Items';
 import backgroundImage from './supermarket.jpeg';
 import { Redirect } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from './context/UserContext';
+
 
 function App() {
-  const { user } = useContext(UserContext);
-
-  if (user) {
-    return <Redirect to="/" />;
-  }
   
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
